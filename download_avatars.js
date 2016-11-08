e0507048a8bb303388f4bdde4c1b19104df61faa
Josh Coles <josh@joshcoles.com>
2016-11-08 02:53:12 +0000

    set up request to pull in github api info for specified user and repo. Assigned own username and access token to variables to use in request

--- download_avatars.js
+++ download_avatars.js
@@ -2,13 +2,19 @@
 var request = require('request');
 
 
-function getRepoContributors(repoOwner, repoName, cb) {
 
+var GITHUB_USER = "joshcoles";
+var GITHUB_TOKEN = "5a1b2ef5aa7146b3f8a13beec33b03ad88dc9eeb";
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL)
 }
 
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors("jensen", "gitfun", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
